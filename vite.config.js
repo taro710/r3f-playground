f0a12a418df34cb0500f41fef57e11a13b716f9d
taro710 <katayamayutaro@team-lab.com>
2024-06-04 18:48:41 +0900

    docs

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  // https://github.com/vitejs/vite-plugin-react/issues/151
-  // https://stackoverflow.com/questions/73815639/how-to-use-jsx-in-a-web-worker-with-vite
+  base: "/r3f-playground/",
   plugins: [react({ fastRefresh: false })],
   worker: {
     plugins: [react({ fastRefresh: false })],
